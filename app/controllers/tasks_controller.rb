ff489bce7ba31488695696b3c24561f7bdbb64df
Erica Gordon & Patrick David <student@epicodus.com>
2014-09-02 16:35:39 -0700

    Create to do list.

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,37 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+    render('tasks/index.html.erb')
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    render('tasks/show.html.erb')
+  end
+
+  def new
+    render('tasks/new.html.erb')
+  end
+
+  def create
+    @task = Task.create(:name => params[:name])
+    render('tasks/success.html.erb')
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+    render('tasks/edit.html.erb')
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(:done => true)
+    render('tasks/complete.html.erb')
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    render('tasks/destroy.html.erb')
+  end
+end
